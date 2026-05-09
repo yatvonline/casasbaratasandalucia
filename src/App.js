4e825d1b6a2ef7da921949cf81e64d12873786f1
yatvonline <dosmaresmagazine@gmail.com>
2026-05-09 12:56:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
-import Landing from "@/pages/Landing";
+import Landing from "@/pages/Landing";import Admin from "@/pages/Admin";
 
 function App() {
   return (
     <div className="App" data-testid="app-root">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<Landing />} /><Route path="/admin" element={<Admin />} />
         </Routes>
       </BrowserRouter>
       <Toaster
